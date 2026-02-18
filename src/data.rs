aea93ec038cb2b51cd847501a3c9fce51795dd30
acaumeil <104687791+acaumeil@users.noreply.github.com>
2026-02-18 14:56:44 +0100

    feat(dependencies): use chrono instead of time

--- src/data.rs
+++ src/data.rs
@@ -1,9 +1,9 @@
-#[cfg(feature = "json")]
-use crate::error;
-
+use chrono::{DateTime, FixedOffset};
 #[cfg(feature = "json")]
 use serde::Serialize;
-use time::OffsetDateTime;
+
+#[cfg(feature = "json")]
+use crate::error;
 
 fn url_encode(input: &str) -> String {
     let mut s = String::new();
@@ -134,7 +134,7 @@ impl Warp10Serializable for Label {
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Data {
-    date: Option<OffsetDateTime>,
+    date: Option<DateTime<FixedOffset>>,
     geo: Option<GeoValue>,
     name: String,
     labels: Vec<Label>,
@@ -143,7 +143,7 @@ pub struct Data {
 
 impl Data {
     pub fn new(
-        date: OffsetDateTime,
+        date: DateTime<FixedOffset>,
         geo: Option<GeoValue>,
         name: String,
         labels: Vec<Label>,
@@ -195,7 +195,7 @@ impl Warp10Serializable for Data {
 
         match self.date {
             Some(date) => {
-                let date_ms = date.unix_timestamp() * 1000000 + (date.microsecond() as Long);
+                let date_ms = date.timestamp_micros();
                 format!(
                     "{}/{} {}{{{}}} {}",
                     date_ms,
@@ -224,7 +224,7 @@ impl Warp10Serializable for Data {
 mod tests {
     use super::*;
 
-    use time::{Duration, OffsetDateTime};
+    use chrono::TimeDelta;
 
     #[test]
     fn serialize_int() {
@@ -279,7 +279,8 @@ mod tests {
     fn serialize_data() {
         assert_eq!(
             Data::new(
-                OffsetDateTime::UNIX_EPOCH + Duration::new(25, 123456789),
+                (chrono::DateTime::UNIX_EPOCH + TimeDelta::new(25, 123456789).unwrap())
+                    .fixed_offset(),
                 None,
                 "original name".to_string(),
                 vec![
@@ -293,7 +294,8 @@ mod tests {
         );
         assert_eq!(
             Data::new(
-                OffsetDateTime::UNIX_EPOCH + Duration::new(25, 123456789),
+                (chrono::DateTime::UNIX_EPOCH + TimeDelta::new(25, 123456789).unwrap())
+                    .fixed_offset(),
                 Some(GeoValue::new(42.66, 32.85, Some(10))),
                 "original name".to_string(),
                 vec![
